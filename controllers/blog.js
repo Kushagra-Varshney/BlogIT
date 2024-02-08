0af6f88a694ae0246ca26652ee934610f08f44be
Kushagra-Varshney <kushagra.ashishvarshney@gmail.com>
2024-02-08 22:42:10 +0530

    fixed blog author bug

--- controllers/blog.js
+++ controllers/blog.js
@@ -10,19 +10,19 @@ async function saveBlogToDB(req, res) {
 
     const { title, body } = req.body;
     const coverImageURL = `/uploads/${req.file.filename}`;
-    const createdBy = req.user._id;
+    const createdBy = req.user.id;
     const blog = await Blog.create({ title, body, createdBy, coverImageURL });
 
     return res.redirect(`/blog/${blog._id}`);
 }
 
 async function getAllBlogs() {
-    const blogs = await Blog.find({}).sort('createdAt');
+    const blogs = await Blog.find({}).sort('createdAt').populate('createdBy');
     return blogs;
 }
-
+ 
 async function getBlogById(req, res) {
-    const blog = await Blog.findById(req.params.id);
+    const blog = await Blog.findById(req.params.id).populate('createdBy');
     return res.render('blog', {
         blog: blog,
         user: req.user
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,19 @@
-const {validateToken} = require('../services/auth');
+const { validateToken } = require('../services/auth');
 
-function checkAuthenticationCookie(cookieName) {  
-  return (req, res, next) => {
-    const token = req.cookies[cookieName];
-    if (!token) {
-      return next();
+function checkAuthenticationCookie(cookieName) {
+    return (req, res, next) => {
+        const token = req.cookies[cookieName];
+        if (!token) {
+            return next();
+        }
+
+        const payload = validateToken(token);
+        if (payload) {
+            req.user = payload;
+        }
+
+        return next();
     }
-
-    const payload = validateToken(token);
-    if(payload){
-      req.user = payload;
-    }
-
-    return next();
-  }
 }
 
 module.exports = {
